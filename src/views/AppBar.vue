37f6d4209fe59dd1144046ed5f9e54291d31c6a6
Benjamin Gautier O <benjamin.gautier.14@sansano.usm.cl>
2020-02-13 12:51:56 -0300

    Componente menu profile en app-bar

--- src/views/AppBar.vue
+++ src/views/AppBar.vue
@@ -5,13 +5,20 @@
                 >
                 <v-app-bar-nav-icon @click="mini = !mini"/>
                 <v-toolbar-title >Application</v-toolbar-title>
+                <v-spacer ></v-spacer>
+                <MenuProfile />
         </v-app-bar>
 </template>
 
 
 <script>  
+import MenuProfile from "./MenuProfile"
+
 export default {
   name: 'AppBar',
+  components:{
+          MenuProfile,
+  },
     data: () => ({
         mini:false,
     }),
---
+++ src/views/MenuProfile.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="text-xs-center">
+    <v-menu
+      attach
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-width="200"
+      left
+      offset-y
+    >
+      <template v-slot:activator="{ on }">
+            <v-btn color="transparent" dark v-on="on"><v-icon>mdi-account</v-icon></v-btn>
+      </template>
+
+      <v-card>
+        <v-list>
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="https://github.com/begandroide/tom-omniscient-vue/blob/readme/tom.png?raw=true" width="50" height="50" alt="Benja">
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{activeUser.name}}</v-list-tile-title>
+              <v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-btn
+                :class="fav ? 'red--text' : ''"
+                icon
+                @click="fav = !fav"
+              >
+                <v-icon>mdi-favorite</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-btn @click="handleLogout()" justify-center color="error"  large dark>
+          Cerrar sesión
+        </v-btn>
+
+      </v-card>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+// import {TokenService} from "../../services/storage.service"
+
+  const MenuProfile = {
+    name: 'MenuProfile',
+    data: function() {
+      return {
+        activeUser: [],
+        fav: true,
+        menu: false,
+        message: false,
+        hints: true
+    };},
+//      async created() {
+//         this.activeUser = await localStorage.token;
+//       },
+//       methods:{
+//       async handleLogout () {  
+//         TokenService.removeToken();  
+//         this.$router.go('/login')  
+//       }  
+//       }
+  }
+  export default MenuProfile; 
+</script>
